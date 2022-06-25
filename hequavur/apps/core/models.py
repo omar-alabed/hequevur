3128f351b1807a4fef398734391e4ec604784439
Omar Alabed <omar.jaser@hotmail.com>
2022-06-25 21:45:28 +0300

    fix the disasterous id override

--- hequavur/apps/core/models.py
+++ hequavur/apps/core/models.py
@@ -1,5 +1,3 @@
-import uuid
-
 from .validators import validate_file_extension
 from .generics import IntegerRangeField
 from django.db import models
@@ -12,17 +10,12 @@ class Department(models.Model):
         return self.department_name
 
 
-def candidate_directory_path(instance, filename):
-    return 'files/{0}/{1}'.format(instance.id, filename)
-
-
 class Candidate(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     full_name = models.CharField(max_length=150)
     date_of_birth = models.DateField()
     years_of_experience = IntegerRangeField(min_value=0, max_value=30, default=0)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    resume = models.FileField(upload_to=candidate_directory_path, validators=[validate_file_extension],
+    resume = models.FileField(upload_to='files/%Y/%m/%d/', validators=[validate_file_extension],
                               max_length=255, null=False, blank=False)
     create_date = models.DateTimeField(auto_now_add=True)
 
--- hequavur/apps/core/tests.py
+++ hequavur/apps/core/tests.py
@@ -1,3 +1,107 @@
-from django.test import TestCase
+from django.urls import path, reverse, include, resolve
+from django.test import SimpleTestCase
+from hequavur.apps.core.views import DepartmentList, DepartmentDetail, FileDetail, CandidateList, CandidateCreate
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
 
-# Create your tests here.
+
+class ApiUrlsTests(SimpleTestCase):
+
+    def test_get_customers_is_resolved(self):
+        url = reverse('department')
+        self.assertEquals(resolve(url).func.view_class, DepartmentList)
+
+
+def _create_test_file(file_path):
+    f = open(file_path, 'w')
+    f.write('test123\n')
+    f.close()
+    f = open(file_path, 'rb')
+    return {'datafile': f}
+
+
+class CoreAPIViewTests(APITestCase):
+    # customers_url = reverse("customer")
+
+    def setUp(self):
+        data = {
+            "department_name": "IT"
+        }
+        response = self.client.post(reverse("department"), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 2)    # {'id': 1, 'department_name': 'IT'}
+
+    def test_create_candidate(self):
+        data = {
+            "full_name": "testo",
+            "date_of_birth": "2022-06-06",
+            "years_of_experience": "3",
+            "department": "1",
+            "resume": _create_test_file('/tmp/test_upload.pdf')
+        }
+        response = self.client.post(reverse("candidate-create"), data, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 5)
+
+
+#     def test_get_customers_authenticated(self):
+#         response = self.client.get(self.customers_url)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#
+#     def test_get_customers_un_authenticated(self):
+#         self.client.force_authenticate(user=None, token=None)
+#         response = self.client.get(self.customers_url)
+#         self.assertEquals(response.status_code, 401)
+#
+#     def test_post_customer_authenticated(self):
+#         data = {
+#             "title": "Mr",
+#             "name": "Peter",
+#             "last_name": "Parkerz",
+#             "gender": "M",
+#             "status": "published"
+#         }
+#         response = self.client.post(self.customers_url, data, format='json')
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#         self.assertEqual(len(response.data), 8)
+#
+#
+# class CustomerDetailAPIViewTests(APITestCase):
+#     customer_url = reverse('customer-detail', args=[1])
+#     customers_url = reverse("customer")
+#
+#     def setUp(self):
+#         self.user = User.objects.create_user(
+#             username='admin', password='admin')
+#         self.token = Token.objects.create(user=self.user)
+#         #self.client = APIClient()
+#         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+#
+#         # Saving customer
+#         data = {
+#             "title": "Mrs",
+#             "name": "Johnson",
+#             "last_name": "MOrisee",
+#             "gender": "F",
+#             "status": "published"
+#         }
+#         self.client.post(
+#             self.customers_url, data, format='json')
+#
+#     def test_get_customer_autheticated(self):
+#         response = self.client.get(self.customer_url)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.data['name'], 'Johnson')
+#
+#     def test_get_customer_un_authenticated(self):
+#         self.client.force_authenticate(user=None, token=None)
+#         response = self.client.get(self.customer_url)
+#         self.assertEqual(response.status_code, 401)
+#
+#     def test_delete_customer_authenticated(self):
+#         response = self.client.delete(self.customer_url)
+#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
