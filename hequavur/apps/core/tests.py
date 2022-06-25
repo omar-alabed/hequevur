b44fdf4b105dd4a8566fc1319267f7704b7a43b9
Omar Alabed <omar.jaser@hotmail.com>
2022-06-25 22:26:10 +0300

    add unit tests

--- hequavur/apps/core/tests.py
+++ hequavur/apps/core/tests.py
@@ -1,30 +1,10 @@
-from django.urls import path, reverse, include, resolve
-from django.test import SimpleTestCase
-from hequavur.apps.core.views import DepartmentList, DepartmentDetail, FileDetail, CandidateList, CandidateCreate
-from rest_framework.test import APITestCase, APIClient
-from rest_framework.authtoken.models import Token
+import os
+from django.urls import reverse
+from rest_framework.test import APITestCase
 from rest_framework import status
-from django.contrib.auth.models import User
-from rest_framework.views import APIView
-
-
-class ApiUrlsTests(SimpleTestCase):
-
-    def test_get_customers_is_resolved(self):
-        url = reverse('department')
-        self.assertEquals(resolve(url).func.view_class, DepartmentList)
-
-
-def _create_test_file(file_path):
-    f = open(file_path, 'w')
-    f.write('test123\n')
-    f.close()
-    f = open(file_path, 'rb')
-    return {'datafile': f}
 
 
 class CoreAPIViewTests(APITestCase):
-    # customers_url = reverse("customer")
 
     def setUp(self):
         data = {
@@ -35,73 +15,25 @@ class CoreAPIViewTests(APITestCase):
         self.assertEqual(len(response.data), 2)    # {'id': 1, 'department_name': 'IT'}
 
     def test_create_candidate(self):
-        data = {
-            "full_name": "testo",
-            "date_of_birth": "2022-06-06",
-            "years_of_experience": "3",
-            "department": "1",
-            "resume": _create_test_file('/tmp/test_upload.pdf')
-        }
-        response = self.client.post(reverse("candidate-create"), data, format='json')
-        print(response.data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(len(response.data), 5)
+        with open(os.path.dirname(__file__)+'/pdf-file.pdf', 'rb') as file:
+            data = {
+                "full_name": "testo",
+                "date_of_birth": "2022-06-06",
+                "years_of_experience": "3",
+                "department": "1",
+                "resume": file
+            }
+            response = self.client.post(reverse("candidate-create"), data)
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            self.assertEqual(len(response.data), 7)
+            # {'id': 1, 'full_name': 'testo', 'date_of_birth': '2022-06-06', 'years_of_experience': 3,
+            # 'resume': s3 link,
+            # 'create_date': date time now, 'department': 1}
 
+    def test_get_candidates_non_authenticated(self):
+        response = self.client.get(reverse("candidate"))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-#     def test_get_customers_authenticated(self):
-#         response = self.client.get(self.customers_url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_get_customers_un_authenticated(self):
-#         self.client.force_authenticate(user=None, token=None)
-#         response = self.client.get(self.customers_url)
-#         self.assertEquals(response.status_code, 401)
-#
-#     def test_post_customer_authenticated(self):
-#         data = {
-#             "title": "Mr",
-#             "name": "Peter",
-#             "last_name": "Parkerz",
-#             "gender": "M",
-#             "status": "published"
-#         }
-#         response = self.client.post(self.customers_url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertEqual(len(response.data), 8)
-#
-#
-# class CustomerDetailAPIViewTests(APITestCase):
-#     customer_url = reverse('customer-detail', args=[1])
-#     customers_url = reverse("customer")
-#
-#     def setUp(self):
-#         self.user = User.objects.create_user(
-#             username='admin', password='admin')
-#         self.token = Token.objects.create(user=self.user)
-#         #self.client = APIClient()
-#         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-#
-#         # Saving customer
-#         data = {
-#             "title": "Mrs",
-#             "name": "Johnson",
-#             "last_name": "MOrisee",
-#             "gender": "F",
-#             "status": "published"
-#         }
-#         self.client.post(
-#             self.customers_url, data, format='json')
-#
-#     def test_get_customer_autheticated(self):
-#         response = self.client.get(self.customer_url)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.data['name'], 'Johnson')
-#
-#     def test_get_customer_un_authenticated(self):
-#         self.client.force_authenticate(user=None, token=None)
-#         response = self.client.get(self.customer_url)
-#         self.assertEqual(response.status_code, 401)
-#
-#     def test_delete_customer_authenticated(self):
-#         response = self.client.delete(self.customer_url)
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    def test_get_file_non_authenticated(self):
+        response = self.client.get(reverse("file-detail", args=[1]))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
--- hequavur/apps/core/urls.py
+++ hequavur/apps/core/urls.py
@@ -3,9 +3,9 @@ from .views import CandidateList, CandidateCreate, DepartmentList,\
     DepartmentDetail, FileDetail
 
 urlpatterns = [
-    path('department/', DepartmentList.as_view()),
-    path('department/<int:pk>/', DepartmentDetail.as_view()),
-    path('candidate/', CandidateList.as_view()),
-    path('candidate/create', CandidateCreate.as_view()),
-    path('file/<int:pk>/', FileDetail.as_view()),
+    path('department/', DepartmentList.as_view(), name='department'),
+    path('department/<int:pk>/', DepartmentDetail.as_view(), name='department-detail'),
+    path('candidate/', CandidateList.as_view(), name='candidate'),
+    path('candidate/create', CandidateCreate.as_view(), name='candidate-create'),
+    path('file/<int:pk>/', FileDetail.as_view(), name='file-detail'),
 ]
